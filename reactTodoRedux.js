4687c3acf731100595076a441d7208b0f7dcc998
unknown <lucas.sartm@gmail.com>
2017-08-30 16:36:17 -0300

    last lesson

--- reactTodoRedux.js
+++ reactTodoRedux.js
@@ -1,3 +1,5 @@
+const { connect } = ReactRedux;
+
 const todo = (state, action) => {
     switch (action.type) {
         case 'ADD_TODO':
@@ -61,44 +63,33 @@ const Link = ({
     );
 };
 
-class FilterLink extends Component {
-
-    componentDidMount() {
-        const { store } = this.context;
-       this.unsubscribe = store.subscribe(() =>
-            this.forceUpdate()
-        );
+const mapStateToLinkProps = (
+    state,
+    ownProps
+) => {
+    return{
+        active: ownProps.filter === state.visibilityFilter
     }
-
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
-
-    render() {
-        const props = this.props;
-        const { store } = this.context;
-        const state = store.getState();
-
-        return (
-            <Link
-                active={
-                    props.filter === state.visibilityFilter
-                }
-                onClick={() =>
-                    store.dispatch({
-                        type: 'SET_VISIBILITY_FILTER',
-                        filter: props.filter
-                    })
-                }
-            >
-                {props.children}
-            </Link>
-        );
-    }
-}
-FilterLink.contextTypes = {
-    store : React.PropTypes.object
 };
+
+const mapDispatchToLinkProps = (
+    dispatch,
+    ownProps
+) => {
+    return {
+        onClick: () => {
+            dispatch({
+                type: 'SET_VISIBILITY_FILTER',
+                filter: ownProps.filter
+            });
+        }
+    };
+}
+
+const FilterLink = connect(
+    mapStateToLinkProps,
+    mapDispatchToLinkProps
+)(Link);
 
 
 const Footer = () => (
@@ -158,7 +149,7 @@ const Todo = ({
         </ul>
 
     );
-    const { connect } = ReactRedux;
+    
     let AddTodo = ({ dispatch }) => {
         let input;
 
